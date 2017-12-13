3db94994b9aca80e33c292e8bb50640d79f608bf
Treyhboy <taruntrehen@gmail.com>
2017-12-14 01:52:47 +0530

    todolist with react completed

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ class App extends Component {
         this.handleclick = this.handleclick.bind(this);
         this.handledel = this.handledel.bind(this);
         this.handleadd = this.handleadd.bind(this);
+        this.handleup = this.handleup.bind(this);
+        this.handledown = this.handledown.bind(this);
 
     }
 
@@ -43,23 +45,43 @@ class App extends Component {
             console.log(n);
             return {item : n,value:''}});
     }
+    handleup(i)
+    {
+        this.setState((prevState) =>
+        {   var n = prevState.item;
+            var m = n.splice(i,1);
+            n.splice(i-1,0,...m);
+            return {item : n,value:''}});
+    }
+    handledown(i)
+    {
+        this.setState((prevState) =>
+        {   var n = prevState.item;
+            var m = n.splice(i,1);
+            n.splice(i+1,0,...m);
+            return {item : n,value:''}});
+    }
+
 
     render() {
 
         return (
+
             <div style={{marginTop:30}}>
+
+                <Row type="flex" align="top" justify="center" ><h1 style={{color:'grey'}}>To Do List </h1></Row>
                 <Row type="flex" align="bottom" justify="left" gutter={16} >
-                    <Col span={8} offset={7} ><Input size="large" placeholder="Enter Todo" value={this.state.value} onChange={this.handlechange} />
+                    <Col span={8} xl={{offset:7}} xs={{offset:5}} ><Input size="large" placeholder="Enter Todo" value={this.state.value} onChange={this.handlechange} />
                 </Col>
                 <Col span={1} ><Button type="primary" size="large" onClick={this.handleadd}><Icon type="plus-circle" /></Button>
                 </Col>
-                <Col span={1} ><Button type="danger" size="large" onClick={this.handledel}>Del</Button>
+                <Col span={1} xs={{offset:2}} xl={{offset:0}}><Button type="danger" size="large" onClick={this.handledel}><Icon type="delete" style={{fontSize:20}}/></Button>
                 </Col>
                 </Row>
                 <div style={{marginTop:100}}>
                 <Row type="flex" align="bottom" justify="left">
-                <Col span={12} offset={6}>
-                <EList item={this.state.item}  delfun={this.handleclick}/>
+                <Col span={12} offset={6} >
+                <EList item={this.state.item}  delfun={this.handleclick} downfun={this.handledown} upfun={this.handleup}/>
                 </Col>
                 </Row>
                 </div>
