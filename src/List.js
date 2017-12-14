243375e18836b5dd84b4acab51aff2f00120aa7e
Treyhboy <taruntrehen@gmail.com>
2017-12-15 03:20:13 +0530

    v1 Completed

--- src/List.js
+++ src/List.js
@@ -1,25 +1,55 @@
-import React from 'react';
-import {List,Icon,Col} from 'antd';
-import 'antd/dist/antd.css';
+import React from "react";
+import { List, Icon, Col } from "antd";
+import "antd/dist/antd.css";
 
-const EList = (props) => {
-    return(<List
-        size="large"
-        bordered
-        dataSource={props.item}
-        renderItem={(val,i)=> <List.Item className="Row" >
-                                <Col span={10}  >
-                                        <span className={val.class} onClick={(event)=>{props.delfun(event.target,i)}} >{val.val}</span>
-                                    </Col>
-                                    <Col span={1} offset={8}>
-                                        {i?<Icon type="up-circle-o" onClick={()=>{props.upfun(i)}} style={{ fontSize: 16, color: '#08c'}} />:"  "}
-                                    </Col>
-                                    <Col span={1} offset={1} xs={{offset:3}}>
-                                        {i===props.item.length-1?" ":<Icon type="down-circle-o" onClick={()=>{props.downfun(i)}} style={{ fontSize: 16, color: '#08c'}} />}
-                                    </Col>
-
-                                </List.Item>}
-    />);
-}
+const EList = props => {
+  return (
+    <List
+      size="large"
+      bordered
+      dataSource={props.item}
+      renderItem={(val, i) => (
+        <List.Item className="Row" key={val.val}>
+          <Col span={10}>
+            <span
+              className={val.class}
+              onClick={event => {
+                props.delfun(event.target, i);
+              }}
+            >
+              {val.val}
+            </span>
+          </Col>
+          <Col span={1} offset={8}>
+            {i ? (
+              <Icon
+                type="up-circle-o"
+                onClick={() => {
+                  props.upfun(i);
+                }}
+                style={{ fontSize: 16, color: "#08c" }}
+              />
+            ) : (
+              "  "
+            )}
+          </Col>
+          <Col span={1} offset={1} xs={{ offset: 3 }}>
+            {i === props.item.length - 1 ? (
+              " "
+            ) : (
+              <Icon
+                type="down-circle-o"
+                onClick={() => {
+                  props.downfun(i);
+                }}
+                style={{ fontSize: 16, color: "#08c" }}
+              />
+            )}
+          </Col>
+        </List.Item>
+      )}
+    />
+  );
+};
 
 export default EList;
